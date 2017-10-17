f9c18b1f4c5c05628d0f23c27258ae1331657bf8
cole alban <colemalban@gmail.com>
2017-10-17 17:48:18 -0400

    Cleaned up directory

--- src/test/java/GetArticlesRequestTest.java
+++ src/test/java/GetArticlesRequestTest.java
@@ -18,7 +18,7 @@ public class GetArticlesRequestTest {
 
     @Before
     public void setup() {
-        key = "NOT A KEY";
+        key = null;
         String sourceString = "{\"id\":\"abc-news-au\",\"name\":\"ABC News (AU)\",\"description\":\"Australia's most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.\",\"url\":\"http://www.abc.net.au/news\",\"category\":\"general\",\"language\":\"en\",\"country\":\"au\",\"urlsToLogos\":{\"small\":\"\",\"medium\":\"\",\"large\":\"\"},\"sortBysAvailable\":[\"top\"]}";
         source = new NewsSource(new JsonParser().parse(sourceString).getAsJsonObject());
     }
